63174c8a5bc44a099e35129f85bb9377554e1734
HuangChunShui <39736300+HuangChunShui@users.noreply.github.com>
2019-12-07 01:18:38 +0800

    Add files via upload
    new project

---
+++ src/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var router = require('./routes');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use('/', router);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
